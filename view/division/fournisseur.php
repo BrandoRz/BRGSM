e1679271a39d8c44645d09aa5eb25b3b2a8da185
BrandoRz <bradorzf99@gmail.com>
2024-08-06 14:01:33 +0300

    ajout fournisseur

--- view/division/fournisseur.php
+++ view/division/fournisseur.php
@@ -5,8 +5,9 @@
   <title>Fournisseur</title>
   <!-- Ajoutez ici les liens vers vos fichiers CSS existants -->
   <link rel="stylesheet" href="path/to/your/css/style.css">
-  <script src="path/to/your/jquery.min.js"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
   <div class="content-header">
@@ -32,7 +33,7 @@
           <div class="card">
             <div class="card-header d-flex align-items-center">
               <h3 class="card-title">Liste des fournisseurs</h3>
-              <button type="button" class="btn btn-primary ml-auto" onclick="location.href='ajouter_fournisseur.php'">Ajouter</button>
+              <button type="button" class="btn btn-primary ml-auto" data-toggle="modal" data-target="#ajouterFournisseurModal">Ajouter</button>
             </div>      
             <div class="card-body">
               <div class="form-group">
@@ -47,13 +48,66 @@
     </div>
   </section>
 
-  <!-- <script type="text/javascript">
+  <!-- Modal Ajouter Fournisseur -->
+  <div class="modal fade" id="ajouterFournisseurModal" tabindex="-1" role="dialog" aria-labelledby="ajouterFournisseurModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="ajouterFournisseurModalLabel">Ajouter Fournisseur</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form id="ajouterFournisseurForm">
+            <div class="form-group">
+              <label for="nomFournisseur">Nom</label>
+              <input type="text" class="form-control" id="nomFournisseur" name="nomFournisseur" required>
+            </div>
+            <div class="form-group">
+              <label for="adresseFournisseur">Adresse</label>
+              <input type="text" class="form-control" id="adresseFournisseur" name="adresseFournisseur" required>
+            </div>
+            <div class="form-group">
+              <label for="contactFournisseur">Contact</label>
+              <input type="text" class="form-control" id="contactFournisseur" name="contactFournisseur" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Ajouter</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Annuler</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      GetListFournisseur('listFournisseur');
+
+      $('#ajouterFournisseurForm').submit(function(e) {
+        e.preventDefault();
+        var formData = $(this).serialize() + '&action=ajouter';
+        $.ajax({
+          type: 'POST',
+          url: 'controller/fournisseur/controller.fournisseur.php',
+          data: formData,
+          success: function(response) {
+            alert(response);
+            $('#ajouterFournisseurModal').modal('hide');
+            GetListFournisseur('listFournisseur');
+          },
+          error: function(xhr, status, error) {
+            alert('Erreur lors de l\'ajout du fournisseur');
+          }
+        });
+      });
+    });
+
     function GetListFournisseurSearch(valeur) {
       if (valeur != "") {
-        var url = "controller/fournisseur/controller.fournisseur.php";
         $.ajax({
           type: "POST",
-          url: url,
+          url: 'controller/fournisseur/controller.fournisseur.php',
           data: {
             getFournisseurSearch: valeur,
           },
@@ -61,9 +115,8 @@
           success: function(res) {
             $("#listFournisseur").html(res);
           },
-          error: function(e) {
-            alert("Erreur");
-            window.location.reload();
+          error: function() {
+            alert("Erreur lors de la recherche");
           }
         });
       } else {
@@ -72,10 +125,9 @@
     }
 
     function GetListFournisseur(output) {
-      var url = "controller/fournisseur/controller.fournisseur.php";
       $.ajax({
         type: "POST",
-        url: url,
+        url: 'controller/fournisseur/controller.fournisseur.php',
         data: {
           getFournisseur: 56,
         },
@@ -83,14 +135,4 @@
         success: function(res) {
           $("#" + output).html(res);
         },
-        error: function(e) {
-          alert("Erreur");
-          window.location.reload();
-        }
-      });
-    }
-
-    GetListFournisseur('listFournisseur');
-  </script> -->
-</body>
-</html>
+        error: function
